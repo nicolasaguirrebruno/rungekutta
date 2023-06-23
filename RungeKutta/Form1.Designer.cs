cec9c0e3acc9cf6ed287546e8aeb5a5840b9af15
= <nicolasaguirrebruno01@gmail.com>
2023-06-23 17:51:57 -0300

    runge

---
+++ RungeKutta/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace RungeKutta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvRunge = new System.Windows.Forms.DataGridView();
+            this.Xi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Yi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.K1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.K2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.K3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.K4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Ximas1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Yimas1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtH = new System.Windows.Forms.TextBox();
+            this.lblH = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtXinicial = new System.Windows.Forms.TextBox();
+            this.lblYfinal = new System.Windows.Forms.Label();
+            this.txtYinicial = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFinalY = new System.Windows.Forms.TextBox();
+            this.btnGenerateRunge = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRunge)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvRunge
+            // 
+            this.dgvRunge.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRunge.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Xi,
+            this.Yi,
+            this.K1,
+            this.K2,
+            this.K3,
+            this.K4,
+            this.Ximas1,
+            this.Yimas1});
+            this.dgvRunge.Location = new System.Drawing.Point(52, 150);
+            this.dgvRunge.Name = "dgvRunge";
+            this.dgvRunge.RowHeadersWidth = 51;
+            this.dgvRunge.RowTemplate.Height = 24;
+            this.dgvRunge.Size = new System.Drawing.Size(1628, 687);
+            this.dgvRunge.TabIndex = 0;
+            // 
+            // Xi
+            // 
+            this.Xi.HeaderText = "Xi";
+            this.Xi.MinimumWidth = 6;
+            this.Xi.Name = "Xi";
+            this.Xi.Width = 125;
+            // 
+            // Yi
+            // 
+            this.Yi.HeaderText = "Yi";
+            this.Yi.MinimumWidth = 6;
+            this.Yi.Name = "Yi";
+            this.Yi.Width = 125;
+            // 
+            // K1
+            // 
+            this.K1.HeaderText = "K1";
+            this.K1.MinimumWidth = 6;
+            this.K1.Name = "K1";
+            this.K1.Width = 125;
+            // 
+            // K2
+            // 
+            this.K2.HeaderText = "K2";
+            this.K2.MinimumWidth = 6;
+            this.K2.Name = "K2";
+            this.K2.Width = 125;
+            // 
+            // K3
+            // 
+            this.K3.HeaderText = "K3";
+            this.K3.MinimumWidth = 6;
+            this.K3.Name = "K3";
+            this.K3.Width = 125;
+            // 
+            // K4
+            // 
+            this.K4.HeaderText = "K4";
+            this.K4.MinimumWidth = 6;
+            this.K4.Name = "K4";
+            this.K4.Width = 125;
+            // 
+            // Ximas1
+            // 
+            this.Ximas1.HeaderText = "Xi+1";
+            this.Ximas1.MinimumWidth = 6;
+            this.Ximas1.Name = "Ximas1";
+            this.Ximas1.Width = 125;
+            // 
+            // Yimas1
+            // 
+            this.Yimas1.HeaderText = "Yi+1";
+            this.Yimas1.MinimumWidth = 6;
+            this.Yimas1.Name = "Yimas1";
+            this.Yimas1.Width = 125;
+            // 
+            // txtH
+            // 
+            this.txtH.Location = new System.Drawing.Point(157, 72);
+            this.txtH.Name = "txtH";
+            this.txtH.Size = new System.Drawing.Size(107, 22);
+            this.txtH.TabIndex = 1;
+            // 
+            // lblH
+            // 
+            this.lblH.AutoSize = true;
+            this.lblH.Location = new System.Drawing.Point(72, 75);
+            this.lblH.Name = "lblH";
+            this.lblH.Size = new System.Drawing.Size(79, 16);
+            this.lblH.TabIndex = 2;
+            this.lblH.Text = "Ingrese el H";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(305, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ingrese el X inicial";
+            // 
+            // txtXinicial
+            // 
+            this.txtXinicial.Location = new System.Drawing.Point(425, 72);
+            this.txtXinicial.Name = "txtXinicial";
+            this.txtXinicial.Size = new System.Drawing.Size(107, 22);
+            this.txtXinicial.TabIndex = 3;
+            // 
+            // lblYfinal
+            // 
+            this.lblYfinal.AutoSize = true;
+            this.lblYfinal.Location = new System.Drawing.Point(569, 75);
+            this.lblYfinal.Name = "lblYfinal";
+            this.lblYfinal.Size = new System.Drawing.Size(115, 16);
+            this.lblYfinal.TabIndex = 6;
+            this.lblYfinal.Text = "Ingrese el Y inicial";
+            // 
+            // txtYinicial
+            // 
+            this.txtYinicial.Location = new System.Drawing.Point(689, 72);
+            this.txtYinicial.Name = "txtYinicial";
+            this.txtYinicial.Size = new System.Drawing.Size(107, 22);
+            this.txtYinicial.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(841, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Ingrese el valor de corte";
+            // 
+            // txtFinalY
+            // 
+            this.txtFinalY.Location = new System.Drawing.Point(998, 75);
+            this.txtFinalY.Name = "txtFinalY";
+            this.txtFinalY.Size = new System.Drawing.Size(107, 22);
+            this.txtFinalY.TabIndex = 7;
+            // 
+            // btnGenerateRunge
+            // 
+            this.btnGenerateRunge.Location = new System.Drawing.Point(1172, 58);
+            this.btnGenerateRunge.Name = "btnGenerateRunge";
+            this.btnGenerateRunge.Size = new System.Drawing.Size(216, 51);
+            this.btnGenerateRunge.TabIndex = 9;
+            this.btnGenerateRunge.Text = "Generar Runge Kutta";
+            this.btnGenerateRunge.UseVisualStyleBackColor = true;
+            this.btnGenerateRunge.Click += new System.EventHandler(this.btnGenerateRunge_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1742, 849);
+            this.Controls.Add(this.btnGenerateRunge);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFinalY);
+            this.Controls.Add(this.lblYfinal);
+            this.Controls.Add(this.txtYinicial);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtXinicial);
+            this.Controls.Add(this.lblH);
+            this.Controls.Add(this.txtH);
+            this.Controls.Add(this.dgvRunge);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRunge)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvRunge;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Xi;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Yi;
+        private System.Windows.Forms.DataGridViewTextBoxColumn K1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn K2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn K3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn K4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ximas1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Yimas1;
+        private System.Windows.Forms.TextBox txtH;
+        private System.Windows.Forms.Label lblH;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtXinicial;
+        private System.Windows.Forms.Label lblYfinal;
+        private System.Windows.Forms.TextBox txtYinicial;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFinalY;
+        private System.Windows.Forms.Button btnGenerateRunge;
+    }
+}
+
---
+++ RungeKutta/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RungeKutta
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class FilaRunge
+        {
+            public double xi;
+            public double yi;
+            public double k1;
+            public double k2;
+            public double k3;
+            public double k4;
+            public double nextXi;
+            public double nextYi;
+
+        }
+
+        private void btnGenerateRunge_Click(object sender, EventArgs e)
+        {
+            GenerateRungeResolution();
+        }
+
+        /// GenerateRungeResolution es el metodo que realiza la ejecucion central de la resolucion
+        private void GenerateRungeResolution() {
+
+            // Declaro las variables h y la condicion de corte que en el programa del aeropuerto van a estar hardcodeados o seran resultado de algun calculo previo
+            double h = double.Parse(txtH.Text);
+            double yFinal = double.Parse(txtFinalY.Text);
+
+            // Declaro la fila que almacenara los valores que luego iran al dgv
+            FilaRunge filaActual = new FilaRunge();
+          
+            // como no uso un ciclo for para detectar la primera iteracion declaro un counter en valor 0
+            int counter = 0;
+
+            do {
+                // Si el counter es cero el x e y seran iguales a los iniciales
+                if (counter == 0)
+                {
+                    filaActual.xi = Convert.ToDouble(txtXinicial.Text);
+                    filaActual.yi = Convert.ToDouble(txtYinicial.Text);
+
+                    // sumo al counter, esto se hara una sola vez porque una sola vez valdra cero para que entre en este if y no gasto recursos sumandole siempre
+                    counter++;
+                }
+                else
+                {   
+                    // Si no es la primera vuelta entonces el valor xi e yi sera igual a los que corresponden a Xsiguiente e Ysiguiente en la fila anterior
+                    filaActual.xi = filaActual.nextXi;
+                    filaActual.yi = filaActual.nextYi;
+                }
+
+                // Obtengo todos los K
+                filaActual.k1 = ObtainK1(filaActual.xi,filaActual.yi);
+                filaActual.k2 = ObtainK23(filaActual.xi, filaActual.yi, h, filaActual.k1);
+                filaActual.k3 = ObtainK23(filaActual.xi, filaActual.yi, h, filaActual.k2);
+                filaActual.k4 = ObtainK4(filaActual.xi,filaActual.yi,h,filaActual.k3);
+
+                // Obtengo los valores siguientes de X e Y
+                filaActual.nextXi = ObtainNextX(filaActual.xi, h);
+                filaActual.nextYi = ObtainNextY(filaActual.yi,h,filaActual.k1,filaActual.k2,filaActual.k3,filaActual.k4);
+
+                // Lo agrego al DGV
+                addToDGV(filaActual);
+
+              // Este while chequea que se haya alcanzado la condicion del ejercicio sino seguimos aplicando metodo runge kutta
+            } while (filaActual.yi < int.Parse(txtFinalY.Text));
+
+            // obtengo la solucion que podria ser tiempo por ejemplo en nuestra simulacion
+            double solution = filaActual.yi;
+
+        }
+
+        private void addToDGV(FilaRunge fila)
+        {
+            // Pasamos a string los valores de la fila para agregarlo al DGV
+            string[] row = {
+                    fila.xi.ToString(),
+                    fila.yi.ToString(),
+                    fila.k1.ToString(),
+                    fila.k2.ToString(),
+                    fila.k3.ToString(),
+                    fila.k4.ToString(),
+                    fila.nextXi.ToString(),
+                    fila.nextYi.ToString()
+                            };
+
+    
+            dgvRunge.Rows.Add(row);
+        }
+
+        private double calculateK( double x,double y) {
+
+            // Reemplazar en el k por la formula de beto, aca uso una de superior para que funcione el metodo nomas
+            // Lo abstrai en un metodo asi podemos variar el calculo del K sin romper los demas metodos
+            double k = 2 * x * y + 6;
+
+            return k;
+        }
+
+        private Tuple<double, double> calculateMidPoint(double x, double y,double h, double k)
+        {
+            // Con esto calculo los puntos medios para obtener posteriormente k2 y k3
+            double xMidPoint = x + (h / 2);
+            double yMidPoint = y + (h / 2 * k);
+
+            Tuple<double, double> midPoints = new Tuple<double, double>(xMidPoint, yMidPoint);
+
+            return midPoints;
+        }
+
+        private double ObtainNextX(double x, double h)
+        {
+            // Siguiente valor de X
+            double x1plus1 = x + h;
+            return x1plus1;
+        }
+
+        private double ObtainNextY(double y, double h, double k1, double k2, double k3, double k4)
+        {
+            // Siguiente valor de Y
+            double y1plus1 = y + ((h / 6) * (k1 + 2*k2 + 2*k3 +k4));
+            return y1plus1;
+        }
+
+
+
+        private double ObtainK1(double x, double y) {
+
+            // K1 directamente aplica formula 
+            double k1 = calculateK(x, y);
+
+            return k1;
+        }
+
+        private double ObtainK23(double x, double y, double h, double k)
+        {
+            //K2 y K3 se calculan de la misma forma nomas que varia el k que le estas mandando por eso lo unifique en un solo metodo ObtainK23
+
+            // Calculo el punto medio
+            Tuple<double, double> midPoints = calculateMidPoint(x, y, h, k);
+
+            double kx = calculateK(midPoints.Item1, midPoints.Item2);
+
+            return kx;
+        }
+
+       
+
+
+        private double ObtainK4(double x, double y, double h, double k3)
+        {
+
+            //Obtengo los valores siguientes y los uso para calcular el k4
+
+            double x1plus1 = ObtainNextX(x,h);
+            double yplushk3 = y + (h*k3);
+
+            double k4 = calculateK(x1plus1,yplushk3);
+
+            return k4;
+        }
+    }
+}
